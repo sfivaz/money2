cc879524ddd538d3d32ed4c6945420feab17c9b0
sfivaz <s.a.fivaz@gmail.com>
2019-08-17 17:44:25 +0200

    fea - rendering html templates with ejs

--- routing/config/custom-express.js
+++ routing/config/custom-express.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
-// const pug = require('pug');
+const ejs = require('ejs').renderFile;
+
+const root = path.join(__dirname + '../../../');
 
 app.use(cors());
 app.use(express.json());
 
-app.set('views', __dirname);
-// app.set('views', __dirname + '/views');
-// app.engine('html', pug);
-// app.set('view engine', 'pug');
+app.use('/css', express.static(root + '/node_modules/bootstrap/dist/css'));
+
+app.set('views', path.join(root + '/app/views'));
+app.engine('html', ejs);
+app.set('view engine', 'ejs');
 
 const routes = require('../routes');
 routes(app);
