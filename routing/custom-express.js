86a47a810cc9dd47f6f119c6528bccdf5904ddfc
sfivaz <s.a.fivaz@gmail.com>
2019-08-18 00:41:10 +0200

    ref - custom-express moved

---
+++ routing/custom-express.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+const ejs = require('ejs').renderFile;
+const routes = require('./routes');
+
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('../webpack.config');
+
+const devServerEnabled = true;
+
+if (devServerEnabled) {
+    //reload=true:Enable auto reloading when changing JS files or content
+    //timeout=1000:Time from disconnecting from server to reconnecting
+    config.entry.app.unshift('webpack-hot-middleware/client?reload=true&timeout=1000');
+
+    //Add HMR plugin
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+    const compiler = webpack(config);
+
+    //Enable "webpack-dev-middleware"
+    app.use(webpackDevMiddleware(compiler, {
+        publicPath: config.output.publicPath
+    }));
+
+    //Enable "webpack-hot-middleware"
+    app.use(webpackHotMiddleware(compiler));
+}
+
+const root = path.resolve(__dirname, '../');
+
+app.use(cors());
+app.use(express.json());
+
+// app.use('/styles', express.static(root + '/app/styles'));
+// app.use('/bundles', express.static(root + '/app/bundles'));
+app.use(express.static('./views'));
+
+app.set('views', path.join(root + '/app/views'));
+app.engine('html', ejs);
+app.set('view engine', 'ejs');
+
+routes(app);
+
+module.exports = app;
