a730fa6e80470a46570f97338e6c373db91ea730
sfivaz <s.a.fivaz@gmail.com>
2019-08-18 03:19:49 +0200

    ref - small ref

--- app/js/builders/PageBuilder.js
+++ app/js/builders/PageBuilder.js
@@ -1,15 +1,12 @@
 export class PageBuilder {
 
     static build(object, Model, View, Controller) {
-        // document.addEventListener("DOMContentLoaded", () => {
+        const model = Object.assign(new Model(), object);
 
-            const model = Object.assign(new Model(), object);
+        const view = new View(model);
 
-            const view = new View(model);
+        const controller = new Controller(model, view);
 
-            const controller = new Controller(model, view);
-
-            controller.start();
-        // });
+        controller.start();
     }
 }
