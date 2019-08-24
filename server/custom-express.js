2c7eb5375936c4a7ba5136cd98e183eae037f35a
sfivaz <s.a.fivaz@gmail.com>
2019-08-24 21:05:50 +0200

    ref - separated file for webpack dev server

--- server/custom-express.js
+++ server/custom-express.js
@@ -6,35 +6,12 @@ const ejs = require('ejs').renderFile;
 const session = require('express-session');
 const routes = require('./routes');
 const AuthHelper = require('./AuthHelper');
+const webpackDevServer = require('./webpack-dev-server');
 
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('../webpack.config');
+const NODE_ENV = 'development';
 
-const devServerEnabled = true;
-
-if (devServerEnabled) {
-    //reload=true:Enable auto reloading when changing JS files or content
-    //timeout=1000:Time from disconnecting from server to reconnecting
-    for (let key in config.entry) {
-        if (config.entry.hasOwnProperty(key))
-            config.entry[key].unshift('webpack-hot-middleware/client?reload=true&timeout=1000');
-    }
-
-    //Add HMR plugin
-    config.plugins.push(new webpack.HotModuleReplacementPlugin());
-
-    const compiler = webpack(config);
-
-    //Enable "webpack-dev-middleware"
-    app.use(webpackDevMiddleware(compiler, {
-        publicPath: config.output.publicPath
-    }));
-
-    //Enable "webpack-hot-middleware"
-    app.use(webpackHotMiddleware(compiler));
-}
+if (NODE_ENV === 'development')
+    webpackDevServer(app);
 
 const root = path.resolve(__dirname, '../');
 
---
+++ server/webpack-dev-server.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('../webpack.config');
+
+module.exports = (app) => {
+
+    //reload=true:Enable auto reloading when changing JS files or content
+    //timeout=1000:Time from disconnecting from server to reconnecting
+    for (let key in config.entry) {
+        if (config.entry.hasOwnProperty(key))
+            config.entry[key].unshift('webpack-hot-middleware/client?reload=true&timeout=1000');
+    }
+
+    //Add HMR plugin
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+    const compiler = webpack(config);
+
+    //Enable "webpack-dev-middleware"
+    app.use(webpackDevMiddleware(compiler, {
+        publicPath: config.output.publicPath
+    }));
+
+    //Enable "webpack-hot-middleware"
+    app.use(webpackHotMiddleware(compiler));
+};
