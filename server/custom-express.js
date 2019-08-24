17be966c379abb550ce8f8dbb0d22b4392549fff
sfivaz <s.a.fivaz@gmail.com>
2019-08-24 21:22:04 +0200

    wip/ref - custom-express

--- server/custom-express.js
+++ server/custom-express.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const cors = require('cors');
-const ejs = require('ejs').renderFile;
-const session = require('express-session');
 const routes = require('./routes');
-const AuthHelper = require('./AuthHelper');
 const webpackDevServer = require('./webpack-dev-server');
 
 const NODE_ENV = 'development';
@@ -13,16 +9,26 @@ const NODE_ENV = 'development';
 if (NODE_ENV === 'development')
     webpackDevServer(app);
 
-const root = path.resolve(__dirname, '../');
-
 app.use(cors());
 app.use(express.json());
+
+/*****************************************************************/
+
+const ejs = require('ejs').renderFile;
+const path = require('path');
+
+const root = path.resolve(__dirname, '../');
 
 app.use('/styles', express.static(root + '/app/styles'));
 
 app.set('views', path.join(root + '/app/views'));
 app.engine('html', ejs);
 app.set('view engine', 'ejs');
+
+/*****************************************************************/
+
+const session = require('express-session');
+const AuthHelper = require('./AuthHelper');
 
 app.set(session({
     secret: 'money',
@@ -45,12 +51,19 @@ app.post('/login', (req, res) => {
         .catch(() => res.send(401));
 });
 
+/*****************************************************************/
+
 routes(app);
+
+/*****************************************************************/
 
 app.use((req, res) =>
     res.status(404).render('./404'));
 
 app.use((err, req, res) =>
     res.status(500).render('./500'));
+
+
+/*****************************************************************/
 
 module.exports = app;
